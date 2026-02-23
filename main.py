fa5c54358fedbd008a86f4f6a48c84f09e6e497e
plvybvckerror <deffhock@gmail.com>
2026-02-23 10:20:15 +0300

    feat:added timer + timer logs

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello World!')
+import time
+
+print('Добро пожаловать, это секундомер!')
+sec = int(input('Введите время, которое необходимо засечь(в секундах)'))
+print(f'Вы ввели {sec} сек.')
+time.sleep(sec)
+print(f'Прошло {sec} сек.')
+
+with open('logs.txt', 'a') as f:
+    f.write(f'Запущен таймер на {sec} сек.\n')
